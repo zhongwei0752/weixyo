54475439937e2912c6728cd5f6e259eb95b6b08b
zhongwei0752 <623610577@qq.com>
2013-08-25 17:04:05 +0800

    1

--- home/wx/wx_index.php
+++ home/wx/wx_index.php
@@ -2,153 +2,107 @@
 /**
   * wechat php test
   */
-include_once './../common.php';
-include_once './wx_common.php';
+
+include_once('./../common.php');
+
 include_once( 'botutil.php' );
-
 //define your token
-
-define("TOKEN", "weixin");
+define("TOKEN", "xiaodongshu");
 $wechatObj = new wechatCallbackapiTest();
-if($_GET["echostr"]){
-$wechatObj->valid();
-}else{
 $wechatObj->responseMsg();
-}
 
 class wechatCallbackapiTest
-{   
-
-     public function valid()
+{
+  public function valid()
     {
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
         if($this->checkSignature()){
-             echo $echoStr;
-             exit;
+          echo $echoStr;
+          exit;
         }
     }
 
-
+  
     public function responseMsg()
     {
-          //get post data, May be due to the different environments
-          $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-          $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
+    //get post data, May be due to the different environments
+    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-          
-
-          //如果是新关注用户
-          if($postObj->Event == 'subscribe'){
-          //send_back(MENU);
-    
-                      $msgType = "news";
-                       $url = "";
-                       $subject="123";
-                       $pic = "123";
-                       $articles[] = makeArticleItem($subject, $subject, $pic, $url); 
-                      } 
-                      $resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, $name,$articles);  
-                     echo $resultStr;
-
-           
-          }
-
-           //extract post data
-          if (!empty($postStr)){
-               
+        //extract post data
+    if (!empty($postStr)){
+                
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
                 $keyword = trim($postObj->Content);
                 $time = time();
-                //判断是否已经有帐号
                 $textTpl = "<xml>
-                                   <ToUserName><![CDATA[%s]]></ToUserName>
-                                   <FromUserName><![CDATA[%s]]></FromUserName>
-                                   <CreateTime>%s</CreateTime>
-                                   <MsgType><![CDATA[%s]]></MsgType>
-                                   <Content><![CDATA[%s]]></Content>
-                                   <FuncFlag>0</FuncFlag>
-                                   </xml>"; 
-
-          
-       
-                    if(!empty( $keyword ))
-                {    
-                           
-                         if ($keyword=='Hello2BizUser'){                             
-                
-                         $contentStr.="过期的通知方式";
-               
-          
-                         
-                         
-                                
-                       
-                        
-                
-                        $msgType = "text";
-                    
-                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                     echo $resultStr;
-                       
-                         }else{
-                 
-                      $msgType = "news";
-
-                       $url = "http://v5.home3d.cn/home/wx/wx.php?uid=$row[uid]&do=feed&num=rand()&wxkey=".$fromUsername."&uid=".$row[uid]."&idtype=".$app_output->data->app[$i]->english;
-                       $subject="123";
-                       $pic = "123";
-
-                       $articles[] = makeArticleItem($subject, $subject, $pic, $url); 
-                      }  
-                       
-                       $articles[] = makeArticleItem($subject, $subject, $pic, $url);
-                         
-                         
-                         
-                       
-                        
-                
-                       
-                    
-                      $resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, $name,$articles);  
-                     echo $resultStr;
-                }
-        }else{
-                     echo "Input something...";
+              <ToUserName><![CDATA[%s]]></ToUserName>
+              <FromUserName><![CDATA[%s]]></FromUserName>
+              <CreateTime>%s</CreateTime>
+              <MsgType><![CDATA[%s]]></MsgType>
+              <Content><![CDATA[%s]]></Content>
+              <FuncFlag>0</FuncFlag>
+              </xml>";             
+        if(!empty( $keyword ))
+                { 
+                    if ($keyword=='Hello2BizUser'){
+            $msgType = "text";
+            $contentStr = "你好！欢迎关注小冬树，小冬树将为你提供部分网站数据，想要更多的数据，请关注我们的官网，目前小冬树有以下功能:
+        回复数字【1】--用药助手;
+        回复数字【2】--查看最新活动;
+        回复数字【3】--查看最新群组话题;
+        回复数字【4】--查看最新案例讨论;
+        回复数字【5】--查看医疗笑话;
+        回复数字【6】--查看今日医疗资讯;
+";
+            $resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr); 
+          }else{
+                     $msgType = "text";
+                     $contentStr = "你好！目前小冬树有以下功能:
+回复数字【1】--用药助手;
+回复数字【2】--查看最新活动;
+回复数字【3】--查看最新群组话题;
+回复数字【4】--查看最新案例讨论;
+回复数字【5】--查看医疗笑话;
+回复数字【6】--查看今日医疗资讯;
+";
+                      $resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr); 
+                      }
+                    }
+                   echo $resultStr;
+                  
+                }else{
+                  echo "Input something...";
                 }
 
         }else {
-             echo "";
-             exit;
+          echo "";
+          exit;
         }
     }
-         
-     private function checkSignature()
-     {
+    
+  private function checkSignature()
+  {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];    
-                 
-          $token = TOKEN;
-          $tmpArr = array($token, $timestamp, $nonce);
-          sort($tmpArr);
-          $tmpStr = implode( $tmpArr );
-          $tmpStr = sha1( $tmpStr );
-         
-          if( $tmpStr == $signature ){
-               return true;
-          }else{
-               return false;
-          }
-     }
-}
+        $nonce = $_GET["nonce"];  
+            
+    $token = TOKEN;
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    
+    if( $tmpStr == $signature ){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
+  
+}
 ?>
