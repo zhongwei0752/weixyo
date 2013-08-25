a33ae1d8cc9d140b59d8e1b86f8784446911be18
zhongwei0752 <623610577@qq.com>
2013-08-25 16:57:35 +0800

    小修复v1.4

--- home/wx/wx_index.php
+++ home/wx/wx_index.php
@@ -40,70 +40,23 @@ class wechatCallbackapiTest
                 $toUsername = $postObj->ToUserName;
                 $keyword = trim($postObj->Content);
                 $time = time();
-          $dbname = "zhongwei";
-          $host = "58.215.187.8";
-          $user = "zhongwei";
-          $pass = "623610577";
+
           
 
           //如果是新关注用户
           if($postObj->Event == 'subscribe'){
           //send_back(MENU);
-                $con = mysql_connect("$host","$user","$pass");
-                              if (!$con)
-                                {
-                                die('Could not connect: ' . mysql_error());
-                                }
-                              mysql_select_db("$dbname", $con);
-                              $result = mysql_query("SELECT s.*,sf.* FROM uchome_space s LEFT JOIN uchome_spacefield sf ON s.uid=sf.uid  WHERE s.wxkey='".$toUsername."' ");
-                              mysql_query("SET NAMES 'utf8'");
-          if($row = mysql_fetch_array($result)){
-            
-            
-            
-          $v5 = mysql_query("SELECT a.*,m.* FROM uchome_appset a LEFT JOIN uchome_menuset m ON a.num=m.menusetid WHERE a.uid='$row[uid]' and a.appstatus='1'");
-                          while($v52= mysql_fetch_array($v5)){
-                               $a[] = "$v52[num]";
-                            $b[] = "$v52[subject]";
-
-                          }
-                         
     
                       $msgType = "news";
-                      if($row[name]){
-                        $name=$row[name];
-                      }else{
-                        $name=$row[username];
-                      }
-                      $appurl = "http://v5.home3d.cn/home/capi/space.php?do=app&uid=$row[uid]";
-                      $app = file_get_contents($appurl,0,null,null);
-                      $app_output = json_decode($app);
-                      $count=$app_output->data->count;
-                      $zhong2 = mysql_query("SELECT * FROM uchome_recommend  WHERE uid='$row[uid]'");
-                      $wei2 = mysql_fetch_array($zhong2);
-                      $name=$wei2[subject];
-                      $url = "http://v5.home3d.cn/home/wx/wx.php?do=home&uid=".$row[uid];
-                      $pic = "http://v5.home3d.cn/home/".$wei2[imageurl];
-                      $articles[] = makeArticleItem($name, $name, $pic, $url);
-                      for($i=0;$i<$count;$i++){
-                       $url = "http://v5.home3d.cn/home/wx/wx.php?uid=$row[uid]&do=feed&num=rand()&wxkey=".$fromUsername."&uid=".$row[uid]."&idtype=".$app_output->data->app[$i]->english;
-                       if($app_output->data->app[$i]->newname){
-                       $subject=$app_output->data->app[$i]->newname;
-                       }else{
-                       $subject=$app_output->data->app[$i]->subject;
-                     }
-                       $pic = "http://v5.home3d.cn/home/".$app_output->data->app[$i]->image1url;
+                       $url = "";
+                       $subject="123";
+                       $pic = "123";
                        $articles[] = makeArticleItem($subject, $subject, $pic, $url); 
                       } 
                       $resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, $name,$articles);  
                      echo $resultStr;
 
-                         }else{
-               $contentStr.="未填写微信id";
-               $msgType = "text";
-               $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-               echo $resultStr;
-        }
+           
           }
 
            //extract post data
@@ -124,104 +77,7 @@ class wechatCallbackapiTest
                                    <FuncFlag>0</FuncFlag>
                                    </xml>"; 
 
-          $con = mysql_connect("$host","$user","$pass");
-                              if (!$con)
-                                {
-                                die('Could not connect: ' . mysql_error());
-                                }
-                              mysql_select_db("$dbname", $con);
-                              $result = mysql_query("SELECT s.*,sf.* FROM uchome_space s LEFT JOIN uchome_spacefield sf ON s.uid=sf.uid  WHERE s.wxkey='".$toUsername."' ");
-                              mysql_query("SET NAMES 'utf8'");
-                            if($row = mysql_fetch_array($result)){
-                            $eventKey = $postObj->EventKey; //取出事件标识
-                            $eventKey = trim($eventKey);
-                            if($eventKey){
-                            $zhong = mysql_query("SELECT a.*,m.* FROM uchome_appset a LEFT JOIN uchome_menuset m ON a.num=m.menusetid WHERE a.uid='$row[uid]'and m.english='$eventKey'");
-                            $wei = mysql_fetch_array($zhong);
-                            if($wei){
-                            $english=$wei['english'];
-                            $fristname="$wei[subject]";
-                            $contentStr.="$english";
-                            }else{
-                            $contentStr.="123";
-                            }
-                      $msgType = "news";
-                      if($row[name]){
-                        $name=$row[name];
-                      }else{
-                        $name=$row[username];
-                      }
-                      $appurl = "http://v5.home3d.cn/home/capi/space.php?do=$english&uid=$row[uid]&page=1&perpage=4";
-                      $app = file_get_contents($appurl,0,null,null);
-                      $app_output = json_decode($app);
-                      $count=$app_output->data->count;
-                      $zhong2 = mysql_query("SELECT * FROM uchome_recommend  WHERE uid='$row[uid]'");
-                      $wei2 = mysql_fetch_array($zhong2);
-                      //$name=$wei2[subject];
-                    $url = "http://v5.home3d.cn/home/wx/wx.php?do=home&uid=".$row[uid];
-                    $pic = "http://v5.home3d.cn/home/".$wei2[imageurl];
-                    $articles[] = makeArticleItem($fristname,$fristname, $pic, $url);
-                      $id=$english."id";
-                      for($i=0;$i<$count;$i++){
-                        if($english=="introduce"){
-                          $englishid=$app_output->data->introduce[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->introduce[$i]->image1url;
-                          $subject=$app_output->data->introduce[$i]->subject;
-                        }elseif($english=="product"){
-                          $englishid=$app_output->data->product[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->product[$i]->image1url;
-                          $subject=$app_output->data->product[$i]->subject;
-                        }elseif($english=="development"){
-                          $englishid=$app_output->data->development[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->development[$i]->image1url;
-                          $subject=$app_output->data->development[$i]->subject;
-                        }elseif($english=="industry"){
-                          $englishid=$app_output->data->industry[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->industry[$i]->image1url;
-                          $subject=$app_output->data->industry[$i]->subject;
-                        }elseif($english=="branch"){
-                          $englishid=$app_output->data->branch[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->branch[$i]->image1url;
-                          $subject=$app_output->data->branch[$i]->subject;
-                        }elseif($english=="cases"){
-                          $englishid=$app_output->data->cases[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->cases[$i]->image1url;
-                          $subject=$app_output->data->cases[$i]->subject;
-                        }elseif($english=="job"){
-                          $englishid=$app_output->data->job[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->job[$i]->image1url;
-                          $subject=$app_output->data->job[$i]->subject;
-                        }elseif($english=="goods"){
-                          $englishid=$app_output->data->goods[$i]->$id;
-                          $pic = "http://v5.home3d.cn/home/".$app_output->data->goods[$i]->image1url;
-                          $subject=$app_output->data->goods[$i]->subject;
-                        }
-                       
-                       $url = "http://v5.home3d.cn/home/wx/wx.php?do=detail&id=$englishid&uid=$row[uid]&idtype=$id&type=$english&viewuid=$row[uid]";
-                       
-                       
-                       $articles[] = makeArticleItem($subject, $subject, $pic, $url); 
-                      }
-                $resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, $name,$articles);  
-               echo $resultStr;             
-          }
-            
-            
-          $v5 = mysql_query("SELECT a.*,m.* FROM uchome_appset a LEFT JOIN uchome_menuset m ON a.num=m.menusetid WHERE a.uid='$row[uid]' and a.appstatus='1'");
-                          while($v52= mysql_fetch_array($v5)){
-                               $a[] = "$v52[num]";
-                            $b[] = "$v52[subject]";
-
-                          }
-
-                         
-        }else{
-           $contentStr.="此微信公众号还未通过验证，微信id:$toUsername,请等待审核";
-               $msgType = "text";
-               $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-               echo $resultStr;
-        }
-                
+          
        
                     if(!empty( $keyword ))
                 {    
@@ -242,53 +98,13 @@ class wechatCallbackapiTest
                      $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                      echo $resultStr;
                        
-                         }elseif($keyword=="绑定"){
-                           $msgType = "news";
-                           $url = "http://v5.home3d.cn/home/wx/wx.php?uid=$row[uid]&do=bind&num=rand()&wxkey=".$fromUsername."&uid=".$row[uid]."";
-                           $subject="输入帐号密码与微信进行绑定";
-                           $pic = "";
-                           $articles[] = makeArticleItem($subject, $subject, $pic, $url);
-                           $resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, $name,$articles);  
-                           echo $resultStr;
-                         }elseif($keyword=="z"){
-                            $msgType = "text";
-                           $contentStr.="$fromUsername";
-                           $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                         echo $resultStr;
-                         }elseif($keyword=="w"){
-                            $msgType = "text";
-                           $contentStr.="$toUsername";
-                           $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                         echo $resultStr;
                          }else{
-                  $zhong = mysql_query("SELECT a.*,m.* FROM uchome_appset a LEFT JOIN uchome_menuset m ON a.num=m.menusetid WHERE a.uid='$row[uid]'and a.num='$keyword'");
-                  $wei = mysql_fetch_array($zhong);
                  
-                    
                       $msgType = "news";
-                      if($row[name]){
-                        $name=$row[name];
-                      }else{
-                        $name=$row[username];
-                      }
-                      $appurl = "http://v5.home3d.cn/home/capi/space.php?do=app&uid=$row[uid]";
-                      $app = file_get_contents($appurl,0,null,null);
-                      $app_output = json_decode($app);
-                      $count=$app_output->data->count;
-                       $zhong2 = mysql_query("SELECT * FROM uchome_recommend  WHERE uid='$row[uid]'");
-                  $wei2 = mysql_fetch_array($zhong2);
-                  $name=$wei2[subject];
-                    $url = "http://v5.home3d.cn/home/wx/wx.php?do=home&uid=".$row[uid];
-                    $pic = "http://v5.home3d.cn/home/".$wei2[imageurl];
-                    $articles[] = makeArticleItem($name, $name, $pic, $url);
-                      for($i=0;$i<$count;$i++){
+
                        $url = "http://v5.home3d.cn/home/wx/wx.php?uid=$row[uid]&do=feed&num=rand()&wxkey=".$fromUsername."&uid=".$row[uid]."&idtype=".$app_output->data->app[$i]->english;
-                       if($app_output->data->app[$i]->newname){
-                       $subject=$app_output->data->app[$i]->newname;
-                       }else{
-                       $subject=$app_output->data->app[$i]->subject;
-                     }
-                       $pic = "http://v5.home3d.cn/home/".$app_output->data->app[$i]->image1url;
+                       $subject="123";
+                       $pic = "123";
 
                        $articles[] = makeArticleItem($subject, $subject, $pic, $url); 
                       }  
